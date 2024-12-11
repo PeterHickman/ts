525db0af4fbfcb1dfe734348bc5687b739cf7d66
Peter Hickman <peterhickman386@googlemail.com>
2024-12-11 18:21:45 +0000

    updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ts
+# ts - A pipe to add a timestamp to lines on STDOUT
+
+Sometimes I run processes that write to stdout. By piping the output through `ts` it adds a timestamp to each line
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BINARY='/usr/local/bin'
+APP=ts
+
+echo "Building $APP"
+go build -ldflags="-s -w" $APP.go
+
+echo "Installing $APP to $BINARY"
+install $APP $BINARY
+
+echo "Removing the build"
+rm $APP
